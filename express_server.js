8a99e8f4b1ad184cc61d39beaccea66f65917eca
clarchiu <clarchiu@gmail.com>
2020-11-10 00:43:17 -0800

    -added urls_new.ejs template for longUrl submission form -added post /urls route to handle request -added body-parser dependency -added random string generator as placeholder for getting shortURLs

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs") 
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -30,9 +33,30 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  let short = generateRandomString();
+  urlDatabase[short] = req.body.longURL;  // Log the POST request body to the console
+  console.log(short);
+  res.end();
+})
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const short = req.params.shortURL;
   const long = urlDatabase[short];
   const templateVars = { shortURL: short, longURL: long, };
   res.render("urls_show", templateVars);
 });
+
+function generateRandomString() {
+  let randomString = '';
+  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i++ ) {
+     randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return randomString;
+}
+

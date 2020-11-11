74877bc24a81fe4ecf353d21b739cdccf0160da1
Clarence Chiu <clarchiu@gmail.com>
2020-11-11 11:08:05 +0000

    refactored test data to database.js module

--- express_server.js
+++ express_server.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+const { urlDatabase, users } = require("./database");
 const app = express();
 const PORT = 8080; // default port 8080
-
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
 
 app.set("view engine", "ejs");
 app.use(cookieParser());
